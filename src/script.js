7bbab4730f9161b8d115ad4296447cb62f18095c
GilsonSantos <gilsonjosert@gmail.com>
2019-09-27 00:51:01 -0400

    first commit

---
+++ src/script.js
@@ -0,0 +1,19 @@
+const app = new Vue({
+  el:'#app',
+  data: {
+    name: 'DOSI',
+  },
+  methods: {
+    showModal(){
+      modal.modal = !modal.modal
+    }
+  }
+})
+
+const modal = new Vue({
+  el:'#modal',
+  data: {
+    modal: false,
+    email: ''
+  }
+})
